f8553bf4e3d339c0805150cd9a96b006e8e328e6
luanyoushu <luanyoushu@xiaoheiban.cn>
2019-08-26 15:00:13 +0800

    节点数据

--- PhoenixiOS/PhoenixiOS/Widgets/HtmlParse/HtmlTools.swift
+++ PhoenixiOS/PhoenixiOS/Widgets/HtmlParse/HtmlTools.swift
@@ -7,7 +7,30 @@
 //
 
 import UIKit
+import SwiftSoup
 
 class HtmlTools: NSObject {
 
+    /// 传入 html 文本和节点名称， 返回 节点数据的 value 值
+    ///
+    /// - Parameters:
+    ///   - html: html 文本
+    ///   - node: div title 等 web 节点
+    /// - Returns: 返回所有节点内部的数据
+    @objc static func getNodes(html: String, node: String) -> [String]? {
+        do {
+            let document = try SwiftSoup.parse(html)
+            var resultItems: [String] = [String]()
+            let elements: Elements = try document.select(node)
+
+            for element in elements {
+                let text = try element.text()
+                resultItems.append(text)
+            }
+
+            return resultItems
+        } catch {
+            return nil
+        }
+    }
 }
